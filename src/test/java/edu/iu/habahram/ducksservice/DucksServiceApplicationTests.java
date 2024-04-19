5545c603e0cb06884acb7bbc80336f817d04d27d
hbahramian <habahram@iu.edu>
2024-04-19 16:51:32 -0400

    .

--- src/test/java/edu/iu/habahram/ducksservice/DucksServiceApplicationTests.java
+++ src/test/java/edu/iu/habahram/ducksservice/DucksServiceApplicationTests.java
@@ -3,10 +3,9 @@ package edu.iu.habahram.ducksservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class DucksServiceApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
